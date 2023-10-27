53cf1a58c3cb9644cc1021cf603f1f76ac73b50d
kulkul <miharja0501@gmail.com>
2023-10-27 11:18:33 +0700

    pertemuan 5

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,6 +18,15 @@
                 </div>
             </div>
 
+      @hasrole('pustakawan')
+        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+        <x-nav-link :href="route('role')" :active="request()-
+        >routeIs('role')">
+        {{ __('Role') }}
+        </x-nav-link>
+        </div>
+    @endhasrole
+
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
